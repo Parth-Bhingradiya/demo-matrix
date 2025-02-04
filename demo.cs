102054535bbdd47c4fa45c71ea22b0d93a70234e
Parth-Bhingradiya <parthbhingradiya91040@gmail.com>
2025-02-04 06:42:02 +0000

    add cs file

---
+++ demo.cs
@@ -0,0 +1,14 @@
+// calculator basic function application   
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//add function 
+public class Calculator
+{
+    public static int add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+}
